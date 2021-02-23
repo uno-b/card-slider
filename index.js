18f67df0ed96bedca8d717c25545813dc4e048ad
Unumandakh <bg.unuu@yahoo.com>
2021-02-23 16:01:09 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var myVar = setInterval(() => {
+            let parent = document.querySelector('.page_hero-talents_items');
+            let children = parent.children;
+            let changeItem = children[0];
+            parent.removeChild(changeItem);
+            parent.appendChild(changeItem);
+        }, 3000);
+
+
